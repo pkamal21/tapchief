d16828852038b0c85eca6862c0ff18e917396552
pkamal21 <31390826+pkamal21@users.noreply.github.com>
2019-12-04 00:39:46 +0530

    Update urls.py

--- tapsearch/urls.py
+++ tapsearch/urls.py
@@ -2,6 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.search_list, name='search_list'),
-    path('post/', views.text_new, name='text_new'),
+    path('', views.text_new, name='text_new'),
 ]
